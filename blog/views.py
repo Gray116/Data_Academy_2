9a2179a8216760f4c35ce7822ad2127c45ec1b06
Gray116 <swkim116@naver.com>
2021-08-31 09:57:09 +0900

    api

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 # DB 액세스한 것과 같다.
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    post = Post.objects.all()
-    context = {'posts':post}
-    return render(request, 'blog/index.html', context)
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+# def index(request):
+#     object_list = Post.objects.all()
+#     context = {'object_list':object_list}
+#     return render(request, 'blog/post_list.html', context)
 
 
 def detail(request, pk):
